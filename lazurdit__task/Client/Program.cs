e4fc4c6932ddecc3bad30ab8cebf9775ceb8d425
hamzaaboras <hamzaaboras@hotmial.com>
2024-04-03 18:31:52 +0300

    Add project files.

---
+++ lazurdit__task/Client/Program.cs
@@ -0,0 +1,18 @@
+global using lazurdit_task1.Shared;
+global using lazurdit_task1.Client.Services.ContactService;
+
+using lazurdit_task1.Client;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using lazurdit__task.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IContactService, ContactService>();
+await builder.Build().RunAsync();
